763d4823776489b7be40811d40646a5c79b1bff1
abzy-iscool <abhishekjani960@gmail.com>
2023-09-19 15:30:21 +0300

    new files added as well as better integration for encapsulation between U.I components

---
+++ src/components/NavDraw.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+
+</script> 
+
+<template>
+<v-navigation-drawer
+          expand-on-hover
+          rail
+        >
+          <v-list>
+            <v-list-item
+              prepend-avatar="logo.png"
+              title="Sandra Adams"
+              subtitle="sandra_a88@gmailcom"
+            ></v-list-item>
+          </v-list>
+  
+          <v-divider></v-divider>
+  
+          <v-list nav variant="flat" mandatory active-color="secondary">
+            <v-list-item lines="one" subtitle="" prepend-icon="home" title="Home" value="home" ></v-list-item>
+            <v-list-item lines="one" subtitle="" prepend-icon="explore" title="Discover" value="discover"></v-list-item>
+            <v-list-item lines="one" subtitle="" prepend-icon="bookmarks" title="Saved" value="saved"></v-list-item>
+            <v-list-item lines="one" prepend-icon="settings" title="Settings" value="settings"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+</template>
+
+<style scoped>
+
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,4 +1,5 @@
 <script setup>
+import NavDraw from '../components/NavDraw.vue';
 import ProductCard from '../components/ProductCard.vue';
 import SearchBar from '../components/SearchBar.vue';
 
@@ -7,27 +8,7 @@ import SearchBar from '../components/SearchBar.vue';
 <template>
     <v-card>
       <v-layout>
-        <v-navigation-drawer
-          expand-on-hover
-          rail
-        >
-          <v-list>
-            <v-list-item
-              prepend-avatar="logo.png"
-              title="Sandra Adams"
-              subtitle="sandra_a88@gmailcom"
-            ></v-list-item>
-          </v-list>
-  
-          <v-divider></v-divider>
-  
-          <v-list nav variant="flat" mandatory active-color="secondary">
-            <v-list-item lines="one" subtitle="" prepend-icon="home" title="Home" value="home" ></v-list-item>
-            <v-list-item lines="one" subtitle="" prepend-icon="explore" title="Discover" value="discover"></v-list-item>
-            <v-list-item lines="one" subtitle="" prepend-icon="bookmarks" title="Saved" value="saved"></v-list-item>
-            <v-list-item lines="one" prepend-icon="settings" title="Settings" value="settings"></v-list-item>
-          </v-list>
-        </v-navigation-drawer>
+        <NavDraw></NavDraw>
         <v-main >
            <SearchBar></SearchBar>
           <ProductCard></ProductCard>
